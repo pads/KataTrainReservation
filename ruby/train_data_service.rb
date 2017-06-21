ff303c0fa14d35d6c811306d38f06c4792a7229f
Ben Paddock <pads@thisispads.me.uk>
2017-06-21 20:11:07 +0100

    Always fetch first seat from real train service

--- ruby/train_data_service.rb
+++ ruby/train_data_service.rb
@@ -1,8 +1,22 @@
+require 'httparty'
+require 'json'
 require_relative 'reservations.rb'
 
 class TrainDataService
+  include HTTParty
+  base_uri 'http://localhost:8081'
+
   def next_available_seat
     # TODO: test and implement
-    Seat.new('A', 1)
+    coach = seat_plan["seats"].values[0]["coach"]
+    seat_number = seat_plan["seats"].values[0]["seat_number"]
+    Seat.new(coach, seat_number)
+  end
+
+  private
+
+  def seat_plan
+    response = self.class.get('/data_for_train/express_2000')
+    JSON.parse!(response.body)
   end
 end
